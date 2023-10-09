2bd875468debdb7419c7150dd649fe2ca51c3e54
Moloko Sehata <moloko4@live.com>
2023-10-09 10:34:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.C__Class_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+}
